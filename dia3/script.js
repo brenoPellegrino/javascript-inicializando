ea739765e1e3c19297fe272b5855ab910060f385
brenoPellegrino <bdpellegrino@gmail.com>
2022-09-23 18:42:01 -0300

    exercicio 4

--- dia3/script.js
+++ dia3/script.js
@@ -1,19 +1,59 @@
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let larger = array[0];
-let smaller = array[0];
+let maiorPrimo = 0;
 
-// console.log(array[0].length);
+function test_prime(n)
+{
 
-for (let index = 1; index < array.length; index++) {
-  if (array[index].length >= larger.length){
-    larger = array[index]
-  } else if (array[index].length <= smaller.length){
-    smaller = array[index];
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;  
   }
 }
 
-console.log('a maior string é ' + larger);
-console.log('a menor string é ' + smaller);
+
+for(let index = 0; index <=50; index++){
+  if (test_prime(index) === true && index > 3){
+    maiorPrimo = index;
+  }
+}
+
+console.log(maiorPrimo);
+
+
+
+
+
+
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let larger = array[0];
+// let smaller = array[0];
+
+// // console.log(array[0].length);
+
+// for (let index = 1; index < array.length; index++) {
+//   if (array[index].length >= larger.length){
+//     larger = array[index]
+//   } else if (array[index].length <= smaller.length){
+//     smaller = array[index];
+//   }
+// }
+
+// console.log('a maior string é ' + larger);
+// console.log('a menor string é ' + smaller);
 
 
 // let word = 'tryber';
